384829260276ace4a5d5d99c45000d3aedada007
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-06-13 19:08:42 +0530

    file changes

--- main.tf
+++ main.tf
@@ -50,5 +50,15 @@ resource "aws_autoscaling_group" "asg" {
     id      = aws_launch_template.template.id
     version = "$Latest"
   }
+
+  dynamic "tag" {
+    for_each = var.tags
+    content {
+      key                 = tag.key
+      propagate_at_launch = true
+      value               = tag.value
+    }
+  }
+
 }
 
