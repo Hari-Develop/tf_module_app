b991121c9ddea3e60460eb2621bae56ea4f29007
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-06-18 16:38:31 +0530

    file changes

--- main.tf
+++ main.tf
@@ -62,3 +62,12 @@ resource "aws_autoscaling_group" "asg" {
 
 }
 
+resource "aws_lb_target_group" "main" {
+  name     = "${var.name}-${var.env}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  tags = merge(var.tags, { Name = "${var.name}-${var.env}-tg" })
+}
+
