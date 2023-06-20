79d864630728ae2a906ebbef4808f76d0fa41434
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-06-20 21:31:19 +0530

    file changes

---
+++ userdata.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+labauto ansible
+
+ansible-pull -i localhost , -U https://github.com/Hari-Develop/roboshop_ansible.git roboshop.yml -e role_name=${name} -e env=${env} &>>/opt/ansible.log 
