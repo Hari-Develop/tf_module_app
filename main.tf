894c861007ed58c84078b432c60624c7f69c24cd
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-06-20 19:25:28 +0530

    file changes

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ resource "aws_lb_listener_rule" "main" {
 }
 resource "aws_route53_record" "main" {
   zone_id = var.domain_id
-  name    = local.lb_dns_name
+  name    = local.dns_name
   type    = "CNAME"
   ttl     = 30
   records = [var.lb_dns_name]
